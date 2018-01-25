158217b0f1cb9566a34cf6d6dfbe83ce92bbf85e
duccuong <cuong09011997@gmail.com>
2018-01-25 15:26:06 +0700

    ket thuc ngay 03

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -8,6 +8,8 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
+    $this->enablePlugins(array('sfDoctrinePlugin'));
+    $this->disablePlugins(array('sfPropelPlugin'));
+    //$this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
   }
 }
