013770ea956ca6d7b35af5bcf6f52b7012e76e1b
duccuong <cuong09011997@gmail.com>
2018-01-26 17:08:40 +0700

    ket thuc ngay 8 uint test

---
+++ test/unit/JobeetTest.php
@@ -0,0 +1,6 @@
+<?php
+// test/unit/JobeetTest.php
+require_once dirname(__FILE__).'/../bootstrap/unit.php';
+ 
+$t = new lime_test(1, new lime_output_color());
+$t->pass('This test always passes.');
